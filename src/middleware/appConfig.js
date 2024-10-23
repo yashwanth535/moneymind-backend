b78961fcf22a7290e21b533e0d9f6fb3886b3e07
yashwanth679 <yashwanth.lumia535@gmail.com>
2024-10-24 00:00:11 +0530

    changed logo link

--- src/middleware/appConfig.js
+++ src/middleware/appConfig.js
@@ -3,31 +3,23 @@ require('dotenv').config();
 const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
-const bodyParser=require("body-parser");
-const mongoose = require('mongoose');
-const User = require('../models/User');
-const getExpenseModel = require('../models/expense');
 const session = require('express-session');
 
 const configureApp = () => {
   const app = express();
 
-  // Set views path
   const viewsPath = path.join(__dirname, '../../template')
   const partialsPath = path.join(__dirname, "../../template/partials");
 
-  console.log(viewsPath);
-  // Set view engine
+
   app.set("view engine", "hbs");
   app.set("views", viewsPath);
-  app.use(express.static(path.join(__dirname, '../../public')));
-  
-  // Register partials
   hbs.registerPartials(partialsPath);
 
-  // Set static files directory
-  app.use(express.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
+  app.use(express.static(path.join(__dirname, '../../public')));
+  app.use(express.urlencoded({ extended: true }));//used when reading directly from form
+  app.use(express.json());//used when parsing in json in client side
+
 
 
    app.use(session({
