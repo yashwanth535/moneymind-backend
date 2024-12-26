798841d43571cea62216045709e21429c6c66439
cse-f <verify.moneymind@gmail.com>
2024-12-27 01:49:10 +0530

    "test commit"

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const {  configureApp } = require("./src/middleware/appConfig");
-const isAuthenticated = require("./src/middleware/isAuthenticated")
-const signinRoutes = require("./src/routes/signin");
-const signupRoutes = require("./src/routes/signup");
-const addExpenseRoutes = require("./src/routes/addExpense")
+const configureApp       = require("./src/middleware/appConfig");
+const isAuthenticated    = require("./src/middleware/isAuthenticated")
+const signinRoutes       = require("./src/routes/signin");
+const signupRoutes       = require("./src/routes/signup");
+const addExpenseRoutes   = require("./src/routes/addExpense")
 const fetchExpenseRoutes = require("./src/routes/fetchExpense")
-const homeRoutes =require("./src/routes/home")
-const landingRoutes=require("./src/routes/landing");
+const homeRoutes         = require("./src/routes/home")
+const landingRoutes      = require("./src/routes/landing");
 
 
 const app = configureApp();
@@ -20,10 +20,7 @@ app.use("/fetch-expenses",isAuthenticated,fetchExpenseRoutes);
 
 
 
-
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`listening to http://localhost:${PORT}`);
-
 });
